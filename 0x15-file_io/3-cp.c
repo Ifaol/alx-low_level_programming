ffa481a09257f2faa98c543b03c03802a88122b6
Ifaol <ifaolhunde@gmail.com>
2023-10-30 05:18:05 -0700

    A programme program that copies the content of a file to another file added

---
+++ 0x15-file_io/3-cp.c
@@ -0,0 +1,82 @@
+#include "main.h"
+/**
+ *main - a function zt copies the content of a file to another file
+ *@argc: no of args entered
+ *@argv: pointer to array of args
+ *
+ *Return: 0 always sucessfull
+ */
+int main(int argc, char *argv[])
+{
+const char *file_from = NULL;
+const char *file_to = NULL;
+if (argc != 3)
+{
+dprintf(STDERR_FILENO, "Usage: cp file_from file_to\n");
+exit(97);
+}
+file_from = argv[1];
+file_to = argv[2];
+copy_file(file_from, file_to);
+return (0);
+}
+/**
+ *error_exit - a function zt prints provided error and exit code
+ *@code: error code
+ *@message: error message
+ *@file: pointer to a file name
+ *
+ *Return: void
+ */
+void error_exit(int code, const char *message, const char *file)
+{
+dprintf(STDERR_FILENO, "Error: %s %s\n", message, file);
+exit(code);
+}
+/**
+ *copy_file - a function zt copies content from one file to other
+ *@file_from: pointer to src file
+ *@file_to: pointer to dest file
+ *
+ *Return: void
+ */
+void copy_file(const char *file_from, const char *file_to)
+{
+int fd_from, fd_to;
+ssize_t bytes_read, bytes_written;
+char buffer[1024];
+fd_from = open(file_from, O_RDONLY);
+if (fd_from == -1)
+{
+error_exit(98, "Can't read from file", file_from);
+}
+fd_to = open(file_to, O_WRONLY | O_CREAT | O_TRUNC, 0664);
+if (fd_to == -1)
+{
+error_exit(99, "Can't write to file", file_to);
+}
+bytes_read = read(fd_from, buffer, 1024);
+while (bytes_read > 0)
+{
+bytes_written = write(fd_to, buffer, bytes_read);
+if (bytes_written == -1)
+{
+error_exit(99, "Can't write to file", file_to);
+}
+bytes_read = read(fd_from, buffer, 1024);
+}
+if (bytes_read == -1)
+{
+error_exit(98, "Can't read from file", file_from);
+}
+if (close(fd_from) == -1)
+{
+dprintf(STDERR_FILENO, "Can't close fd %d\n", fd_from);
+exit(100);
+}
+if (close(fd_to) == -1)
+{
+dprintf(STDERR_FILENO, "Can't close fd %d\n", fd_to);
+exit(100);
+}
+}
