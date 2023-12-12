28ce43f4b95bfe3859925e4c00d3d658529ad1c3
Ifaol <ifaolhunde@gmail.com>
2023-12-12 06:21:15 -0800

    make me win sh updated

--- 0x18-dynamic_libraries/101-make_me_win.sh
+++ 0x18-dynamic_libraries/101-make_me_win.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
-wget -P /tmp https://github.com/Ifaol/alx-low_level_programming/blob/master/0x18-dynamic_libraries/win.so
-export LD_PRELOAD=/tmp/win.so
+wget -P .. https://github.com/Ifaol/alx-low_level_programming/blob/master/0x18-dynamic_libraries/win.so
+export LD_PRELOAD="$PWD/ ../win.so"
