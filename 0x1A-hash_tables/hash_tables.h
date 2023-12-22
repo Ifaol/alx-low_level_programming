9a77cfdcfe5bda240f8d63949b0dce3d3a5c5f5e
Ifaol <ifaolhunde@gmail.com>
2023-12-22 02:48:20 -0800

    A function that creates a hash table added

--- 0x1A-hash_tables/hash_tables.h
+++ 0x1A-hash_tables/hash_tables.h
@@ -1,6 +1,7 @@
 #ifndef HASH_TABLES_H
 #define HASH_TABLES_H
 #include <stdio.h>
+#include <stdlib.h>
 /**
  * struct hash_node_s - Node of a hash table
  *
@@ -29,4 +30,5 @@ typedef struct hash_table_s
      unsigned long int size;
      hash_node_t **array;
 } hash_table_t;
+hash_table_t *hash_table_create(unsigned long int size);
 #endif
